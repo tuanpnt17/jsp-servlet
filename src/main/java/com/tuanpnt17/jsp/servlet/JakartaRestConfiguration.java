220d55e4c1b3ea23d7fd516743956177925cd916
Trong Tuan <phamnguyentrongtuan13@gmail.com>
2023-12-05 17:22:58 +0700

    first commit

---
+++ src/main/java/com/tuanpnt17/jsp/servlet/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.tuanpnt17.jsp.servlet;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/tuanpnt17/jsp/servlet/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.tuanpnt17.jsp.servlet.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
